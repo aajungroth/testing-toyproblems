dbfddeab829d5f35e6ef14e783618da56f548ccf
aajungroth <aajungroth@ucdavis.edu>
2017-07-11 10:11:14 -0700

    Rewrote rockPaperScissors

--- _rockPaperScissors/src.js
+++ _rockPaperScissors/src.js
@@ -17,95 +17,46 @@
 *
 */
 
-//input a accumulator and a counter
-//output boolean based on test
-//procedure test coutner to see if it equals 2
-var checkCounters = function(accumulator, counter) {
-  if (accumulator === false) {
-    return false;
-  } else if (counter < 2) {
-    return false
-  } else {
-    return true;
-  }
-};
-
-//input  a counter
-//output the permutations
-//procedure return the correct term
-  //rock = 0, paper = 1, scissors = 2
-var convertCounters = function(counter) {
-  if (counter === 0) {
-    return 'rock';
-  } else if (counter === 1) {
-    return 'paper';
-  } else if (counter === 2) {
-    return 'scissors';
-  }
-  return '';
-};
-
-//input none
-//output should return all permutations of throws
-  //in a 3 round game of rock paper scissors
-  //as an array of arrays of strings
-//procedure should recursively generate the permutations
+//output an array of every valid sequence of rock, paper, and scissors
+//input the number of rounds
+//constraints none stated
+//edge cases zero rounds should return an empty array
+  //undefined rounds should default to 3
 var rockPaperScissors = function(rounds) {
-  //The index of the total number of counters needed
-  var totalCountersMaxIndex = 2;
-  //Array of base three counters
-  var counters = [0, 0, 0];
-  //Array of permutations
-  var permutations = [];
-  //A new permutation
-  var permutation = [];
-
-  //handles any number of rounds
-  if ((rounds !== undefined) && (rounds > 0)) {
-    totalCountersMaxIndex = rounds - 1;
-    counters = [];
-    for (var i = 0; i < rounds; i++) {
-      counters.push(0);
+  if ((rounds === 0) || (rounds === undefined)) {
+    rounds = 3;
+  }
+  var results = [];
+  var depth = 0;
+  var currentSequence = [];
+  //inner recursive function
+  var findSequences = function(depth) {
+    //if the depth is greater than or equal to rounds
+    if (depth >= rounds) {
+      //add the current sequence to results
+      results.push(currentSequence.slice());
+    } else {
+      //push rock onto the current sequence
+      currentSequence.push('rock');
+      //call inner recursive function with depth +1
+      findSequences(depth + 1);
+      //pop rock off the current sequence
+      currentSequence.pop();
+      //push paper onto the current sequence
+      currentSequence.push('paper');
+      //call inner recursive functionwith depth +1
+      findSequences(depth + 1);
+      //pop paper off the current sequence
+      currentSequence.pop();
+      //push scissors onto the current sequence
+      currentSequence.push('scissors');
+      //call inner recursive function with depth + 1
+      findSequences(depth + 1);
+      //pop scissors off the current sequence
+      currentSequence.pop();
     }
   }
-
-  //while loop
-  while (counters.reduce(checkCounters, true) === false) {
-    //stop condition
-    //if all counters are at 2
-
-    //else
-    //add permutation of rock paper or scissors to the
-      // permutations array based on the counters
-    permutation = counters.map(convertCounters);
-    permutations.push(permutation);
-
-    //inner recursive function
-    var incrementCounters = function (index) {
-      //increment the counter
-      counters[index]++;
-
-      //base case
-      //The counter is less than three
-
-      //recursive case
-      //if the counter is equal to three
-      if (counters[index] > 2) {
-        //set it equal to 0
-        counters[index] = 0;
-        //recurse on the next counter
-        index--;
-        incrementCounters(index);
-      }
-    }
-    incrementCounters(totalCountersMaxIndex);
-  }
-  //add final permutation
-  permutation = counters.map(convertCounters);
-  permutations.push(permutation);
-
-  //return array of permutations
-  return permutations;
+  //call inner recursive function
+  findSequences(depth);
+  return results;
 };
-
-console.log(rockPaperScissors(4));
